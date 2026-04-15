b9c9b4ba2fe4d0fecc8c09d6eb738bce9c3e6fca
Andres Luna <miosesfreddy123@gmail.com>
2026-04-15 23:16:39 +0200

    creacion de carrito de compra con despegable latera y envio de pedido a la base de datos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
-import { Component, signal, inject } from '@angular/core'; // Añadimos inject
-import { ProductosComponent } from './components/productos/productos';
+import { Component, inject, signal } from '@angular/core'; // Añadimos inject
 import { CarritoComponent } from "./components/carrito/carrito";
+import { ProductosComponent } from './components/productos/productos';
 import { CartService } from './services/cart.service'; // Asegúrate de que la ruta sea correcta
 
 @Component({
@@ -16,4 +16,11 @@ export class App {
   // 1. Inyectamos el servicio
   // 2. Lo ponemos como 'public' para que el HTML lo vea
   public cartService = inject(CartService); 
+
+ // Dentro de tu clase AppComponent
+isCartOpen = false;
+
+toggleCart() {
+  this.isCartOpen = !this.isCartOpen;
+}
 }

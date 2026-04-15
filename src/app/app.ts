6ac06d20b305c3369a188fbe9570e15ff8185e1b
Andres Luna <miosesfreddy123@gmail.com>
2026-04-16 01:19:29 +0200

    feat:
    - Añadido sistema de videos divididos (Split Video) en el footer con autoplay.
    - Configurada carpeta public para gestión de assets de video.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core'; // Añadimos inject
+import { AfterViewInit, Component, inject, signal } from '@angular/core'; // Añadimos inject
 import { CarritoComponent } from "./components/carrito/carrito";
 import { ProductosComponent } from './components/productos/productos';
 import { CartService } from './services/cart.service'; // Asegúrate de que la ruta sea correcta
@@ -10,12 +10,21 @@ import { CartService } from './services/cart.service'; // Asegúrate de que la r
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements AfterViewInit {
+
+  ngAfterViewInit() {
+    // Busca todos los videos y dales "play" manualmente por si acaso
+    const videos = document.querySelectorAll('video');
+    videos.forEach(video => {
+      video.muted = true;
+      video.play().catch(error => console.log("El autoplay fue bloqueado, pero está listo:", error));
+    });
+  }
   protected readonly title = signal('tienda-ropa-app');
 
   // 1. Inyectamos el servicio
   // 2. Lo ponemos como 'public' para que el HTML lo vea
-  public cartService = inject(CartService); 
+  public cartService = inject(CartService);
 
  // Dentro de tu clase AppComponent
 isCartOpen = false;

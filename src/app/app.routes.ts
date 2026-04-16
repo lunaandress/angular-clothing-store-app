72e4bcd4e03e92170a1fa3d5054f85c6153de62d
Andres Luna <miosesfreddy123@gmail.com>
2026-04-16 14:20:00 +0200

    "feat: añadir acceso de usuario al navbar
    - Creado contenedor 'nav-right-section' para agrupar iconos globales.
    - Añadido icono de usuario (Login) con navegación via routerLink.
    - Aplicado Flexbox para alinear perfectamente el Login y el Carrito.
    - Ajustados espacios y efectos hover para mantener la estética minimalista."

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home'; // Cambiado a HomeComponent
 import { ProductosComponent } from './components/productos/productos';
+import { LoginComponent } from './components/login/login';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }, 
+    { path: '', component: HomeComponent },
     { path: 'shop', component: ProductosComponent },
+    { path: 'login', component : LoginComponent},
     { path: '**', redirectTo: '' }
 ];

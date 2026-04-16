0fd2439ed536bd7ed841b28333a8003e2a3e53c5
Andres Luna <miosesfreddy123@gmail.com>
2026-04-16 13:25:48 +0200

    "feat: añadir preloader animado con branding de marca
    - Implementada estructura de preloader en index.html para carga instantánea.
    - Añadida animación CSS 'flip3D' para el logo STYLES® en el eje Y.
    - Configurada lógica en app.ts para desvanecer el preloader tras la carga inicial.
    - Refinados tiempos de transición para una experiencia de usuario fluida y premium."

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,6 @@ import { CartService } from './services/cart.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  // IMPORTANTE: Quitamos ProductosComponent de aquí porque se carga vía rutas
   imports: [CarritoComponent, RouterLink, RouterOutlet], 
   templateUrl: './app.html',
   styleUrl: './app.css'
@@ -14,11 +13,29 @@ import { CartService } from './services/cart.service';
 export class App implements AfterViewInit {
 
   ngAfterViewInit() {
+    // 1. Lógica de videos
     const videos = document.querySelectorAll('video');
     videos.forEach(video => {
       video.muted = true;
       video.play().catch(error => console.log("Autoplay listo", error));
     });
+
+    // 2. Ejecutar la desaparición del preloader
+    this.ocultarPreloader();
+  }
+
+  // Nueva función para quitar el logo giratorio
+  private ocultarPreloader() {
+    const preloader = document.getElementById('preloader');
+    if (preloader) {
+      // Le damos 1.5 segundos para que el usuario vea el logo girar con clase
+      setTimeout(() => {
+        preloader.classList.add('fade-out');
+        
+        // Lo borramos del código después de que termine la transición de 0.6s
+        setTimeout(() => preloader.remove(), 400);
+      }, 1500);
+    }
   }
 
   protected readonly title = signal('tienda-ropa-app');
